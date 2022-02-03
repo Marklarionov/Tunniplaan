5a0c58883d5c4bf1bc968dff74c4f0f9642af2d8
opilane <opilane@HP-CZC6297B8L>
2022-02-03 12:36:59 +0200

    l

--- Tunniplaan.py
+++ Tunniplaan.py
@@ -1,43 +1,61 @@
 from tkinter import * 
 from tkinter.messagebox import *
-def failist_sõna():
-    tund_kirjeldus={}
-    file=open("Tunnid_kirjeldused.txt","r")
-    for line in file:
-        tund,kirjeldus=line.strip().split(":")
-        tund_kirjeldus[tund.strip()]=kirjeldus.strip()
-    file.close()
-    print(tund_kirjeldus)
-    return tund_kirjeldus
+def failist_sõnastikusse():
+	tund_kirjeldus={}
+	file=open("Tunnid_kirjeldused.txt","r")
+	for line in file:
+		tund,kirjeldus=line.strip().split(":")
+		tund_kirjeldus[tund.strip()]=kirjeldus.strip()
+	file.close()
+	print(tund_kirjeldus)
+	return tund_kirjeldus
 
+tund_kirjeldus=failist_sõnastikusse()
 def kirjeldus_akn(t:str):
-    if(askyesno("Küsimus","Kas tahad kirjeldust näha?")):
-        other_win=Toplevel()
-        lbl_kirjeldus=Label(other_win,text=tund_kirjeldus[t]).pack()
-        c=Canvas(other_win,height=500,width=500)
-        file=PhotoImage(file=t)
-        c.create_image(10,10,image=file,acnhor=NW)
-        c.pack()
-        other_win.geometry("400x400")
-        other_win.mainloop()
-    else:
-        showinfo("Vastus","Kui ei taha, siis ei taha")
-    print(tund_kirjeldus[t])
-        
-tund_kirjeldus=failist_sõna()
-win=Tk()
-win.title("Tunniplaan")
-win.geometry("1300x800")
+	if (askyesno("Küsimus","Kas tahad kirjeldust näha?")):
+		alam_aken=Toplevel()
+		lbl_kirjeldus=Label(alam_aken,text=tund_kirjeldus[t]).pack()
+		alam_aken.geometry("600x600")
+		c=Canvas(alam_aken,height=500,width=500)
+		file=PhotoImage(file=t)
+		c.create_image(10,10,image=file,anchor=NW)
+		c.pack()
+		alam_aken.mainloop()
+	else:
+		showinfo("Vastus","Kui ei taha,siis ei taha!")
 
+j=0
+def veel():
+	global j
+	if j==0:
+		aken.geometry(str(win.winfo_width())+"x"+str(aken.winfo_height()-350))
+		btn_veel.config(text="+")
+		j=1
+	else:
+		aken.geometry(str(win.winfo_width())+"x"+str(aken.winfo_height()+350))
+		btn_veel.config(text="-")
+		j=0
+win=Tk()
+win.geometry("1200x800")
+win.title("Tunniplan")
 p=["Esmaspäev","Teisipäev","Kolmapäev","Neljapäev","Reede"]
 j=0
-for i in range(1,10,2):
-    Ep=Label(win,text=p[j],relief="solid").grid(row=i,column=0,rowspan=2,sticky=N+S+W+E)
-    j+=1
-
-kell=["7:40-8:25","8:30-9:15","9:20-10:05","10:10-10:55","11:00-11:45","11:50-12:35","12:40-13:25","13:30-14:15","14:20-15:05","15:10-15:55","16:00-16:45"]
+for i in range(2,11,2):
+	Ep=Label(win,text=p[j],font="Arial 20",relief="groove").grid(row=i,column=0,rowspan=2,sticky=N+S+W+E)
+	j+=1
+keel=["7:40-8:25","8:30-9:15","9:20-10:05","10:10-10:55","11:00-11:45","11:50-12:35","12:40-13:25","13:30-14:15","14:20-15:05","15:10-15:55","16:00-16:45"]
+k=0
+for i in range(1,12,1):
+	Ek=Label(win,text=keel[k],font="Arial 6",relief="flat").grid(row=1,column=i,rowspan=2,sticky=N)
+	k+=1
+Rp=Label(win,text="TARpv21",font="Arial 16",relief="flat").grid(row=0,column=0,sticky=N+S+W+E)
+Ep=Label(win,text="",relief="flat").grid(row=1,column=0,rowspan=1,sticky=N+S+W+E)
 for i in range(11):
-    tn=Label(win,text=str(i)+"\n"+kell[i],relief="solid").grid(row=0,column=i,rowspan=2,sticky=N+S+W+E)
+	tn="t"+str(i)
+	tn=Label(win,text=i,font="Arial 20",relief="groove").grid(row=0,column=i+1,sticky=N+S+W+E)
+
+btn_veel=Button(win,text="+", font="Calibri 26",bg="green",command=veel)
+btn_veel.place(x=1060,y=400)
     
 
 
@@ -85,12 +103,11 @@ t9=Label(win,text="9",font="Arial 15",width=8,height=3,bg="White",relief="solid"
 t10=Label(win,text="10",font="Arial 15",width=8,height=3,bg="White",relief="solid").grid(row=0,column=11,sticky=N+S+W+E)
 
 #ESMAPSPÄEV
-#O=Label(win,text="",font="Arial 15",bg="White",relief="solid").grid(row=1,rowspan=2,column=1,sticky=N+S+W+E)
-MultiBtn=Button(win,text="Multimeedia",command=lambda:kirjeldus_akn("sun.gif","Multimeedia"),font="Arial 15",width=8,height=2,bg="#7297FF",relief="sunken").grid(row=1,column=2,columnspan=2,sticky=N+S+W+E)
-MultiBtn=Button(win,text="Multimeedia",command=lambda:kirjeldus_akn("Multimeedia"),font="Arial 15",width=8,height=2,bg="#7297FF",relief="sunken").grid(row=2,column=5,columnspan=2,sticky=N+S+W+E)
-ProgBtn=Button(win,text="Programmerimise alused",command=lambda:kirjeldus_akn("Programmerimise alused"),font="Arial 15",width=8,height=2,bg="#4DD1FA",relief="sunken").grid(row=2,column=2,columnspan=3,sticky=N+S+W+E)
-ProgBtn=Button(win,text="Programmerimise alused",command=lambda:kirjeldus_akn("Programmerimise alused"),font="Arial 15",width=8,height=2,bg="#4DD1FA",relief="sunken").grid(row=1,column=5,columnspan=3,sticky=N+S+W+E)
-Ruhmbtn=Button(win,text="Rühma\njuhataja\n tund",command=lambda:kirjeldus_akn("Programmerimise alused"),font="Arial 15",width=8,height=2,bg="#4DD1FA",relief="sunken").grid(row=1,column=8,rowspan=2,sticky=N+S+W+E)
+MultiBtn=Button(win,text="Multimeedia",command=lambda:kirjeldus_akn("sun.gif"),font="Arial 15",width=8,height=2,bg="#7297FF",relief="sunken").grid(row=1,column=2,columnspan=2,sticky=N+S+W+E)
+MultiBtn=Button(win,text="Multimeedia",command=lambda:kirjeldus_akn("sun.gif"),font="Arial 15",width=8,height=2,bg="#7297FF",relief="sunken").grid(row=2,column=5,columnspan=2,sticky=N+S+W+E)
+ProgBtn=Button(win,text="Programmerimise alused",command=lambda:kirjeldus_akn("prog.png"),font="Arial 15",width=8,height=2,bg="#4DD1FA",relief="sunken").grid(row=2,column=2,columnspan=3,sticky=N+S+W+E)
+ProgBtn=Button(win,text="Programmerimise alused",command=lambda:kirjeldus_akn("prog.png"),font="Arial 15",width=8,height=2,bg="#4DD1FA",relief="sunken").grid(row=1,column=5,columnspan=3,sticky=N+S+W+E)
+Ruhmbtn=Button(win,text="Rühma\njuhataja\n tund",command=lambda:kirjeldus_akn("Ruhmajuhataja tund"),font="Arial 15",width=8,height=2,bg="#4DD1FA",relief="sunken").grid(row=1,column=8,rowspan=2,sticky=N+S+W+E)
 
 #TEISIPÄEV
 IngBtn=Button(win,text="Inglise Keel",command=lambda:kirjeldus_akn("Inglise Keel"),font="Arial 15",width=8,height=2,bg="#F9F2E1",relief="sunken").grid(row=3,column=2,columnspan=2,sticky=N+S+W+E)
